3862552a1219eb18fb12ac82966aec898670531a
Fabrice MWANAFUNZI <mwanafunzifabrice@gmail.com>
2023-01-20 17:23:45 +0200

    #Chore[project SETUP]
    - creating .gitignore
    - creating prettier.js
    - creating .env.exapmle
    
    ##[WIP LOGIN SETUP]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-
+import express from 'express'
 import dotenv from 'dotenv'
-import express, {Router} from 'express';
 import mongoose from 'mongoose';
 import contactRoutes from "./routes/contact";
-import signupRoutes from "./routes/singup";
+import userRoutes from "./routes/user";
 import loginRoutes from "./routes/login";
 import blogRoutes  from "./routes/blog"
+const docs = require('./documentation/');
+const swaggerUI = require("swagger-ui-express");
+const {userSpec, loginSpec, contactSpec, blogSpec} = require('./documentation/swagger');
+
 dotenv.config();
 mongoose.set('strictQuery', true);
 mongoose.connect(`mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PASSWORD}@my-brand-fabrice.7myztk9.mongodb.net/?retryWrites=true&w=majority`,{ useNewUrlParser: true })
@@ -14,9 +17,18 @@ mongoose.connect(`mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PA
     const PORT = process.env.PORT;
     app.use(express.json())
     app.use("/api", contactRoutes)
-    app.use("/api", signupRoutes)
-   app.use("/api", loginRoutes)
-   app.use("/api", blogRoutes)
+    app.use("/api", userRoutes)
+    app.use("/api", loginRoutes)
+    app.use("/api", blogRoutes)
+    app.use('/api-doc',swaggerUI.serve,swaggerUI.setup(docs));
+
+
+
+  /*------------------------ by default*--------------------*/
+   app.use((req, res) => {
+    res.status(404).send({ error: 'Endpoint not found' });
+  });
+  /*------------------------ by default*--------------------*/
 
 
     app.listen(PORT, ()=>{
