ebfb4a912c2db29aa3ff33804fcd6ece04d13d1d
Fabrice MWANAFUNZI <mwanafunzifabrice@gmail.com>
2023-01-23 17:17:14 +0200

    Chore[project SETUP]
    - creating initializing server
    
    ##[PROJECT SETUP]

--- src/index.js
+++ src/index.js
@@ -9,13 +9,27 @@ const docs = require('./documentation/');
 const swaggerUI = require("swagger-ui-express");
 const {userSpec, loginSpec, contactSpec, blogSpec} = require('./documentation/swagger');
 
+
+// const documentation = require('./helper/documentations');
+// const components = require('./helper/definitions');
+// const swaggerUi = require('swagger-ui-express');
+// const options = Object.assign({}, documentation, components);
+// const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerSpec = swaggerJsdoc(options);
+
+
 dotenv.config();
 mongoose.set('strictQuery', true);
+    const app  = express();
+
 mongoose.connect(`mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PASSWORD}@my-brand-fabrice.7myztk9.mongodb.net/?retryWrites=true&w=majority`,{ useNewUrlParser: true })
 .then(()=>{
-    const app  = express();
+    // const app  = express();
     const PORT = process.env.PORT;
     app.use(express.json())
+    // app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
     app.use("/api", contactRoutes)
     app.use("/api", userRoutes)
     app.use("/api", loginRoutes)
@@ -35,3 +49,6 @@ mongoose.connect(`mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PA
         console.log(`server started port ${PORT}...`);
     })
 })
+
+
+module.exports = { app };
