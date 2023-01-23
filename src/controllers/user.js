2c36d834e4ce8c3935b31e66c4f7aa84cd019261
Fabrice MWANAFUNZI <mwanafunzifabrice@gmail.com>
2023-01-23 17:30:45 +0200

    Chore[project SETUP]
    - validatin user end points with JWT
    
    ##[PROJECT SETUP]

---
+++ src/controllers/user.js
@@ -0,0 +1,120 @@
+import express from 'express'
+import bcrypt  from 'bcrypt';
+import User  from "../models/user"
+
+
+
+
+
+/*=====================================================REGISTER USER==============================================*/
+
+export const registerUser = async(req,res)=>{
+    try{
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const hashedComfirmPassword = await bcrypt.hash(req.body.confirmPassword, saltRounds);
+
+        // check if the email or username is already registered
+        const existingUser = await User.findOne({ $or: [{email: req.body.email}, {username: req.body.username}] });
+        if (existingUser) {
+            res.status(400).send({ error: 'Email or username already exists' });
+            return;
+        }
+
+        // Check if password and confirmPassword match
+        const passwordsMatch = await bcrypt.compare(req.body.password, hashedComfirmPassword);
+        if (!passwordsMatch) {
+            res.status(400).send({ error: 'Password and Confirm Password do not match' });
+            return;
+        }
+
+        // check if gender is either Male or Female
+        if (req.body.gender !== 'Male' && req.body.gender !== 'Female') {
+            res.status(400).send({ error: 'Invalid Gender' });
+            return;
+        }
+
+        // validate email
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailRegex.test(req.body.email)) {
+            res.status(400).send({ error: 'Invalid Email' });
+            return;
+        }
+
+        // validate username length
+        if (req.body.username.length < 3 || req.body.username.length > 15) {
+            res.status(400).send({ error: 'Username must be between 3 and 15 characters' });
+            return;
+        }
+       
+        const post = new User({
+            email:req.body.email,
+            username:req.body.username,
+            gender:req.body.gender,
+            password:hashedPassword,
+            confirmPassword:hashedComfirmPassword
+        });
+        await post.save()
+        res.send(post)
+    }catch{
+        res.status(404)
+        res.send({error:"Postman not found"})
+    }
+};
+/*=====================================================REGISTER USER==============================================*/
+
+
+/*=====================================================GET ALL USER==============================================*/
+
+export const getUser = async(req,res)=>{
+    try{
+        const query = await User.find()
+        res.send(query)
+    }catch{
+        res.status(404)
+        res.send({error:"Postman not found"})
+    }
+
+}
+/*=====================================================GET ALL USER==============================================*/
+
+
+/*=====================================================DELETE SINGLE USER==============================================*/
+
+export const deleteUser = async(req,res)=>{
+    try{
+
+     // check if user exists
+     const userToDelete = await User.findOne({_id: req.params.id});
+     if (!userToDelete) {
+         res.status(404).send({ error: 'user not found' });
+         return;
+     }
+
+        await User.deleteOne({_id: req.params.id})
+        res.status(200).json({
+            message: 'User deleted successfully'
+        });
+        }catch(error){
+        res.status(404).send({error:"user not found"})
+    }
+}
+
+/*=====================================================DELETE SINGLE USER==============================================*/
+
+
+
+/*=====================================================COUNT ALL USER==============================================*/
+
+export const countUser = async (req, res) => {
+    try {
+        // Count the number of blogs in the collection
+        const userCount = await User.countDocuments();
+
+        res.status(200).send({ message: `There are ${userCount} users in the collection.` });
+    } catch (err) {
+        res.status(500).send({ error: "Error counting users" });
+    }
+}
+
+/*=====================================================COUNT ALL USER==============================================*/
